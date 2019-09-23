60b34ff3ef3baf6dc81061d8c96e839b42040055
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-09-23 11:15:00 +0300

    refactor(browser) SidebarStore

--- packages/browser/src/stores/SidebarStore/SidebarStore.test.ts
+++ packages/browser/src/stores/SidebarStore/SidebarStore.test.ts
@@ -2,12 +2,12 @@ import SidebarStore from 'stores/SidebarStore/SidebarStore';
 describe('ThemeStore', (): void => {
   const sibebarStore = new SidebarStore();
   it('theme should be light as default', (): void => {
-    expect(sibebarStore.getActive).toBeFalsy();
+    expect(sibebarStore.isActive).toBeFalsy();
   });
   it('toggleTheme should switch between true and false', (): void => {
     sibebarStore.toggleIsActive();
-    expect(sibebarStore.getActive).toBeTruthy();
+    expect(sibebarStore.isActive).toBeTruthy();
     sibebarStore.toggleIsActive();
-    expect(sibebarStore.getActive).toBeFalsy();
+    expect(sibebarStore.isActive).toBeFalsy();
   });
 });
--- packages/browser/src/stores/SidebarStore/SidebarStore.ts
+++ packages/browser/src/stores/SidebarStore/SidebarStore.ts
@@ -2,11 +2,7 @@ import { observable, action } from 'mobx';
 import { persist } from 'mobx-persist';
 
 class SidebarStore {
-  @persist @observable private isActive: boolean = false;
-
-  public get getActive() {
-    return this.isActive;
-  }
+  @persist @observable public isActive: boolean = false;
   @action public toggleIsActive(): void {
     this.isActive = !this.isActive;
   }
