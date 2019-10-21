8f0138e19f4cc9ff7f88d6ee3aba4406480fd51d
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-10-21 20:26:10 +0300

    test(browser) refactors TweetBar tests

--- packages/browser/src/components/Tweet/TweetBar/index.test.tsx
+++ packages/browser/src/components/Tweet/TweetBar/index.test.tsx
@@ -14,17 +14,63 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 mockedAxios.patch.mockReturnValueOnce(
   Promise.resolve({ data: {}, status: 200 }),
 );
+mockedAxios.delete.mockResolvedValue({ data: {}, status: 204 });
 
 describe('TweetBar', () => {
   const updateUser = jest.fn();
   const setNotification = jest.fn();
   const setModalState = jest.fn();
-  const deleteTweetHandle = jest.fn();
-  it('render TweetBar', async () => {
-    // expect.assertions(6);
-    const { rerender, getByTestId, queryByTestId } = render(
+  const deleteTweetHandler = jest.fn();
+  const notification = {
+    type: 'warning',
+    content: 'Log in or Sign up to perform this action!',
+  };
+
+  afterEach(() => jest.resetAllMocks());
+  it('render TweetBar (bookmark button)', async () => {
+    expect.assertions(4);
+    const { rerender, getByTestId } = render(
       <TweetBar
-        deleteTweetHandler={deleteTweetHandle}
+        deleteTweetHandler={deleteTweetHandler}
+        setModalState={setModalState}
+        authState={defaultAuthState}
+        tweet={tweet}
+        updateUser={updateUser}
+        setNotification={setNotification}
+      />,
+
+      {
+        wrapper: ({ children }) => <TestWrapper children={children} />,
+      },
+    );
+    const bookmarkButton = getByTestId('bookmark-button');
+    UserEvent.click(bookmarkButton);
+
+    expect(setNotification).toHaveBeenCalledTimes(1);
+    expect(setNotification).toHaveBeenCalledWith(notification);
+
+    rerender(
+      <TweetBar
+        deleteTweetHandler={deleteTweetHandler}
+        setModalState={setModalState}
+        authState={authenticatedAuthState}
+        tweet={tweet}
+        updateUser={updateUser}
+        setNotification={setNotification}
+      />,
+    );
+
+    UserEvent.click(bookmarkButton);
+    await wait(() => {
+      expect(axios.patch).toHaveBeenCalledTimes(1);
+    });
+    expect(updateUser).toHaveBeenCalledTimes(1);
+  });
+  it('render TweetBar (like button)', async () => {
+    expect.assertions(4);
+    const { rerender, getByTestId } = render(
+      <TweetBar
+        deleteTweetHandler={deleteTweetHandler}
         setModalState={setModalState}
         authState={defaultAuthState}
         tweet={tweet}
@@ -37,24 +83,14 @@ describe('TweetBar', () => {
       },
     );
     const likeButton = getByTestId('heart-button');
-    const replyButton = getByTestId('comment-button');
-    const retweetButton = getByTestId('retweet-button');
-    const bookmarkButton = getByTestId('bookmark-button');
-    const deleteButton = queryByTestId('trash-button');
-    const editButtonButton = queryByTestId('editButton-button');
-
     UserEvent.click(likeButton);
-    UserEvent.click(bookmarkButton);
-    UserEvent.click(replyButton);
-    UserEvent.click(retweetButton);
 
-    expect(deleteButton).toBeFalsy();
-    expect(editButtonButton).toBeFalsy();
-    expect(setNotification).toHaveBeenCalledTimes(4);
+    expect(setNotification).toHaveBeenCalledTimes(1);
+    expect(setNotification).toHaveBeenCalledWith(notification);
 
     rerender(
       <TweetBar
-        deleteTweetHandler={deleteTweetHandle}
+        deleteTweetHandler={deleteTweetHandler}
         setModalState={setModalState}
         authState={authenticatedAuthState}
         tweet={tweet}
@@ -64,14 +100,174 @@ describe('TweetBar', () => {
     );
 
     UserEvent.click(likeButton);
-    UserEvent.click(bookmarkButton);
-    UserEvent.click(replyButton);
+    await wait(() => {
+      expect(axios.patch).toHaveBeenCalledTimes(1);
+    });
+    expect(updateUser).toHaveBeenCalledTimes(1);
+  });
+  it('render TweetBar (retweet button)', async () => {
+    expect.assertions(4);
+    const { rerender, getByTestId } = render(
+      <TweetBar
+        deleteTweetHandler={deleteTweetHandler}
+        setModalState={setModalState}
+        authState={defaultAuthState}
+        tweet={tweet}
+        updateUser={updateUser}
+        setNotification={setNotification}
+      />,
+
+      {
+        wrapper: ({ children }) => <TestWrapper children={children} />,
+      },
+    );
+    const retweetButton = getByTestId('retweet-button');
     UserEvent.click(retweetButton);
 
-    await wait(() => {
-      expect(axios.patch).toHaveBeenCalledTimes(2);
+    expect(setNotification).toHaveBeenCalledTimes(1);
+    expect(setNotification).toHaveBeenCalledWith(notification);
+
+    rerender(
+      <TweetBar
+        deleteTweetHandler={deleteTweetHandler}
+        setModalState={setModalState}
+        authState={authenticatedAuthState}
+        tweet={tweet}
+        updateUser={updateUser}
+        setNotification={setNotification}
+      />,
+    );
+
+    UserEvent.click(retweetButton);
+
+    expect(setModalState).toHaveBeenCalledTimes(1);
+    expect(setModalState).toHaveBeenCalledWith('tweetForm', {
+      retweetedId: tweet._id,
+      type: 'retweet',
     });
-    expect(setModalState).toHaveBeenCalledTimes(2);
-    expect(updateUser).toHaveBeenCalledTimes(2);
+  });
+  it('render TweetBar (reply button)', async () => {
+    expect.assertions(4);
+    const { rerender, getByTestId, queryByTestId } = render(
+      <TweetBar
+        deleteTweetHandler={deleteTweetHandler}
+        setModalState={setModalState}
+        authState={defaultAuthState}
+        tweet={tweet}
+        updateUser={updateUser}
+        setNotification={setNotification}
+      />,
+
+      {
+        wrapper: ({ children }) => <TestWrapper children={children} />,
+      },
+    );
+    let replyButton = queryByTestId('reply-button');
+
+    expect(replyButton).toBeFalsy();
+
+    rerender(
+      <TweetBar
+        deleteTweetHandler={deleteTweetHandler}
+        setModalState={setModalState}
+        authState={authenticatedAuthState}
+        tweet={tweet}
+        updateUser={updateUser}
+        setNotification={setNotification}
+      />,
+    );
+    replyButton = getByTestId('comment-button');
+
+    expect(replyButton).toBeTruthy();
+
+    UserEvent.click(replyButton);
+
+    expect(setModalState).toHaveBeenCalledTimes(1);
+    expect(setModalState).toHaveBeenCalledWith('tweetForm', {
+      replyId: tweet._id,
+      type: 'reply',
+    });
+  });
+  it('render TweetBar (edit button)', async () => {
+    expect.assertions(4);
+    const { rerender, getByTestId, queryByTestId } = render(
+      <TweetBar
+        deleteTweetHandler={deleteTweetHandler}
+        setModalState={setModalState}
+        authState={defaultAuthState}
+        tweet={tweet}
+        updateUser={updateUser}
+        setNotification={setNotification}
+      />,
+
+      {
+        wrapper: ({ children }) => <TestWrapper children={children} />,
+      },
+    );
+    let editButton = queryByTestId('edit-button');
+
+    expect(editButton).toBeFalsy();
+
+    rerender(
+      <TweetBar
+        deleteTweetHandler={deleteTweetHandler}
+        setModalState={setModalState}
+        authState={authenticatedAuthState}
+        tweet={tweet}
+        updateUser={updateUser}
+        setNotification={setNotification}
+      />,
+    );
+    editButton = getByTestId('edit-button');
+
+    expect(editButton).toBeTruthy();
+
+    UserEvent.click(editButton);
+
+    expect(setModalState).toHaveBeenCalledTimes(1);
+    expect(setModalState).toHaveBeenCalledWith('tweetForm', {
+      tweet,
+      type: tweet.type,
+    });
+  });
+  it('render TweetBar (delete button)', async () => {
+    expect.assertions(3);
+    const { rerender, getByTestId, queryByTestId } = render(
+      <TweetBar
+        deleteTweetHandler={deleteTweetHandler}
+        setModalState={setModalState}
+        authState={defaultAuthState}
+        tweet={tweet}
+        updateUser={updateUser}
+        setNotification={setNotification}
+      />,
+
+      {
+        wrapper: ({ children }) => <TestWrapper children={children} />,
+      },
+    );
+    let deleteButton = queryByTestId('trash-button');
+
+    expect(deleteButton).toBeFalsy();
+
+    rerender(
+      <TweetBar
+        deleteTweetHandler={deleteTweetHandler}
+        setModalState={setModalState}
+        authState={authenticatedAuthState}
+        tweet={tweet}
+        updateUser={updateUser}
+        setNotification={setNotification}
+      />,
+    );
+    deleteButton = getByTestId('trash-button');
+
+    expect(deleteButton).toBeTruthy();
+
+    UserEvent.click(deleteButton);
+
+    await wait(() => {
+      expect(axios.delete).toHaveBeenCalledTimes(1);
+    });
   });
 });
