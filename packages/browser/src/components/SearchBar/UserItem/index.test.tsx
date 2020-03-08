6e5b992ccf7be384539b1b46fb0793519af8d8d7
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2020-03-08 20:59:13 +0200

    test(browser) fix UserItem test

--- packages/browser/src/components/SearchBar/UserItem/index.test.tsx
+++ packages/browser/src/components/SearchBar/UserItem/index.test.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import user from 'testUtilities/user';
+import TestWrapper from 'testUtilities/TestWrapper';
 import UserItem from './index';
 
 describe('UserItem', () => {
   it('render UserItem', () => {
     expect.assertions(1);
 
-    const { container } = render(<UserItem user={user} />);
+    const { container } = render(<UserItem user={user} />, {
+      wrapper: ({ children }) => <TestWrapper>{children}</TestWrapper>,
+    });
 
     expect(container).toMatchSnapshot();
   });
