a28527510d1a7f11d60bdcd581f7c57323086176
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2022-08-12 11:13:06 +0300

    deploy

--- packages/server/src/middleware/errorHandler.ts
+++ packages/server/src/middleware/errorHandler.ts
@@ -2,7 +2,12 @@ import { Response, Request, NextFunction } from 'express';
 import RESTError from '../utilities/RESTError';
 import logger from '../utilities/logger';
 
-const errorHandler = (error: RESTError, _req: Request, res: Response): void => {
+const errorHandler = (
+  error: RESTError,
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
   const status = error.status || 500;
   const { message, data } = error;
   const resData = data ? { data, message } : { message };
